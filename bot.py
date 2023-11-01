5333063b3a7fb88dc83d45194648822ae0734b20
Trisha <77016955+trishaa4144@users.noreply.github.com>
2023-11-01 19:33:46 -0400

    use yaml to store article history instead of json

--- bot.py
+++ bot.py
@@ -2,11 +2,11 @@ import discord
 import feedparser
 import os
 import asyncio
-import json
+import yaml
 
 BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
-article_history_file = 'article_history.json'
-
+article_history_file = 'article_history.yml'
+article_history = set()
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -14,21 +14,15 @@ intents.message_content = True
 client = discord.Client(intents=intents)
 
 def load_article_history():
-    """
-    Load article history from article_history_file
-    """
     try:
         with open(article_history_file, 'r') as file:
-            return json.load(file)
-    except (FileNotFoundError, json.JSONDecodeError):
-        return []
+            return set(yaml.safe_load(file))
+    except (FileNotFoundError, yaml.YAMLError):
+        return set()
 
-def save_article_history(article_history):
-    """
-    Save history to article_history_file
-    """
+def save_article_history():
     with open(article_history_file, 'w') as file:
-        json.dump(article_history, file)
+        yaml.dump(list(article_history), file)
 
 @client.event
 async def on_ready():
@@ -38,20 +32,20 @@ async def on_ready():
     # Every minute, check for new articles. If a new article has been posted, send the
     # link to the article to the first text channel in the Discord server
     while True:
-        await first_channel.send("fetching rss feed ...")
-
-        article_history = load_article_history()
+        print("fetching rss feed ...")
 
         feed = feedparser.parse('https://dbknews.com/feed/')
 
         for entry in feed.entries:
             if entry.link not in article_history:
-                await first_channel.send(f"new article just dropped: {entry.title}\n{entry.link}")
-                article_history.append(entry.link)
+                print(f"\"{entry.title}\" has not been seen yet, notifying.")
+                await first_channel.send(f"New article from The Diamondback!:\n**{entry.title}**\n{entry.link}")
+                # We can use GPT to summarize the article contents (key: entry[content][value]) here
+                article_history.add(entry.link)
         
-        save_article_history(article_history)
+        save_article_history()
 
-        await asyncio.sleep(60)  # Sleep for 60 seconds
+        await asyncio.sleep(60 * 5)  # Sleep for 60 seconds
 
 
 @client.event
@@ -62,4 +56,11 @@ async def on_message(message):
     if message.content.startswith('$search'):
         await message.channel.send('searching')
 
-client.run(BOT_TOKEN)
+if __name__ == '__main__':
+    article_history = load_article_history()
+    try:
+        client.run(BOT_TOKEN)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        save_article_history()
